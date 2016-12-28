0ddd4f0f5315de34e310eaa17f77e15fd62cceb6
Ross Altman <knowbodynos@gmail.com>
2016-12-28 01:39:11 -0500

    Fixed copy_template.bash.

--- scripts/copy_template.bash
+++ scripts/copy_template.bash
@@ -4,9 +4,9 @@ template=$1
 primerpath=$2
 primername=$3
 
-cp -r "${template}" "${primerpath}/${primername}"
+cp -r "${template}" "${primerpath}/"
 
-files=$(find "${primerpath}/${primername}/" -mindepth 1 -type f)
+files=$(find "${primerpath}/" -mindepth 1 -type f)
 for file in ${files}
     do
     	sed -i "s/template/${primername}/g" ${file}
