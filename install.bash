7a7ad7e4fd8f0be7056a85eadb6f6650acc7bac9
Ross Everett Altman <altman.ro@discovery2.neu.edu>
2017-11-07 00:40:51 -0500

    Updated mpi4py.

--- install.bash
+++ install.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-initfile=$(math -noprompt -run "WriteString[\$Output,\$UserBaseDirectory<>\"/Kernel/init.m\"];Exit[];")
+initfile=$(math -noprompt -run "WriteString[\$Output,\$UserBaseDirectory<>\"/Kernel/init.m\"];Exit[];" 2>/dev/null)
 echo $(cat $initfile | grep -v "SLURMONGO_ROOT") > $initfile
 echo "AppendTo[\$Path, Environment[\"SLURMONGO_ROOT\"]<>\"/packages/Mathematica/cohomCalg\"]" >> $initfile
 echo "AppendTo[\$Path, Environment[\"SLURMONGO_ROOT\"]<>\"/packages/Mathematica/MongoLink\"]" >> $initfile
@@ -13,8 +13,19 @@ rm -r ${pythonpath}/pymongo 2>/dev/null
 git clone git://github.com/mongodb/mongo-python-driver.git pymongo
 wait
 cd pymongo
+git checkout 280efd2d726aa589b8656031f3453d0a7903e1ff
 python setup.py install --user --record filespy.txt
 sage --python setup.py install --user --record filessage.txt
+
+cd ${pythonpath}
+rm -r ${pythonpath}/mpi4py 2>/dev/null
+git clone https://github.com/mpi4py/mpi4py.git mpi4py
+wait
+cd mpi4py
+git checkout edd4ada44f343ffdfee9d09be5de28d9623216d8
+python setup.py install --user --record filespy.txt
+sage --python setup.py install --user --record filessage.txt
+
 cd ${pythonpath}/mongolink
 python setup.py install --user --record filespy.txt
 sage --python setup.py install --user --record filessage.txt
--- packages/python/mpi4py-master/src/mpi4py/futures/_spawn.py
+++
@@ -1,7 +0,0 @@
-# Author:  Lisandro Dalcin
-# Contact: dalcinl@gmail.com
-"""Entry point for spawned workers."""
-from . import _worker
-
-if __name__ == '__main__':
-    _worker.server_spawn()
