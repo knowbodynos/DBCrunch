efbafaa145800a6aad399794a433fc6284c7777f
Ross Everett Altman <altman.ro@discovery2.neu.edu>
2018-01-10 00:20:08 -0500

    Updated crunch analyze.

--- bin/tools/analyze.py
+++ bin/tools/analyze.py
@@ -35,16 +35,17 @@ def timestamp2unit(timestamp, unit = "seconds"):
 
 def seconds2timestamp(seconds):
     timestamp = ""
-    days = str(seconds / (60 * 60 * 24))
+    days = str(seconds / (60 * 60 * 24)).zfill(2)
     remainder = seconds % (60 * 60 * 24)
     hours = str(remainder / (60 * 60)).zfill(2)
     remainder = remainder % (60 * 60)
     minutes = str(remainder / 60).zfill(2)
     remainder = remainder % 60
     seconds = str(remainder).zfill(2)
-    if days != "0":
-        timestamp += days + "-"
-    timestamp += hours + ":" + minutes# + ":" + seconds
+    #if days != "0":
+    #    timestamp += days + "-"
+    #timestamp += hours + ":" + minutes# + ":" + seconds
+    timestamp += days + ":" + hours
     return timestamp
 
 def create_y_ticker(yscale, yexp):
@@ -82,7 +83,9 @@ job_min_max = {}
 #max_time = 0
 
 proc = np.zeros(0, dtype = int)
+proc_done = np.zeros(0, dtype = int)
 write = np.zeros(0, dtype = int)
+write_done = np.zeros(0, dtype = int)
 
 print("Analyzing resource usage statistics...")
 
@@ -106,7 +109,9 @@ for log_file_path in iglob(in_path + "/*.log"):
                         id_times[log_id] = log_time
                         if proc.shape[0] <= log_time:
                             proc.resize(log_time + 1)
+                            proc_done.resize(log_time + 1)
                         proc[log_time - log_duration:log_time + 1] += np.ones(log_duration + 1, dtype = int)
+                        proc_done[log_time] += 1
                         #if min_time == None or log_time - log_duration < min_time:
                         #    min_time = log_time - log_duration
                         if job_min_max[log_job][0] == None or log_time - log_duration < job_min_max[log_job][0]:
@@ -114,7 +119,9 @@ for log_file_path in iglob(in_path + "/*.log"):
                     elif log_state == "OUT":
                         if write.shape[0] <= log_time:
                             write.resize(log_time + 1)
+                            write_done.resize(log_time + 1)
                         write[id_times[log_id] + 1:log_time + 1] += np.ones(log_time - id_times[log_id], dtype = int)
+                        write_done[log_time] += 1
                         #if log_time > max_time:
                         #    max_time = log_time
                         if log_time > job_min_max[log_job][1]:
@@ -126,15 +133,17 @@ for log_file_path in iglob(in_path + "/*.log"):
 #    max_time = min_time
 max_time = max(proc.shape[0], write.shape[0])
 proc.resize(max_time)
+proc_done.resize(max_time)
 write.resize(max_time)
+write_done.resize(max_time)
 steps = proc + write
 
 min_time = (steps > 0).argmax()
 steps = np.delete(steps, range(min_time))
 proc = np.delete(proc, range(min_time))
+proc_done = np.delete(proc_done, range(min_time))
 write = np.delete(write, range(min_time))
-
-print(max(proc))
+write_done = np.delete(write_done, range(min_time))
 
 jobs = np.zeros(steps.shape[0], dtype = int)
 times = np.arange(steps.shape[0], dtype = int)
@@ -146,14 +155,11 @@ for j in job_min_max.values():
 
 #plot_labels = ["# Jobs", "# Steps", "Processing", "Processed", "Writing", "Written"]
 #plot_lists = [jobs, steps, run, temp, wait, out]
-plot_labels = ["Processing", "Writing", "In Use"]
-plot_lists = [proc, write, steps]
-colors = ['blue', 'red', 'purple']
 
 #dpi = 10
 #xmargin = 0
 #ymargin = 0
-xscale = 10
+xscale = int(times[-1] / (60 * 60))
 yscale = 5
 #xpixels  = xscale * (max_time - min_time + 1)
 #ypixels = [yscale * max(p) for p in plot_lists]
@@ -161,60 +167,75 @@ yscale = 5
 
 print("Plotting resource usage statistics...")
 
-fig, axarr = plt.subplots(2, sharex = True, figsize = (10, 8))#, dpi = dpi)
-plt.suptitle('Resource Usage Statistics for ' + modname + "_" + controllername)
-plt.xlabel('Time (HH:MM)')
+width, height = 15., 8.
+linewidth = width / times[-1]
 
-axarr[0].grid(color = 'k', linestyle = '-', linewidth = 0.01)
-axarr[0].xaxis.set_major_locator(MultipleLocator(xscale * 60 * 60))
-axarr[0].xaxis.set_major_formatter(FuncFormatter(lambda x, pos: seconds2timestamp(int(x/xscale))))
-axarr[0].xaxis.set_minor_locator(MultipleLocator(xscale * 60))
-axarr[0].set_xlim(0, xscale * max(times))
+fig, axarr = plt.subplots(4, sharex = True, figsize = (width, height))#, dpi = dpi)
+plt.suptitle('Resource Usage Statistics for ' + modname + "_" + controllername)
+plt.xlabel('Time (DD:HH)')
+
+for i in range(len(axarr)):
+    axarr[i].grid(color = 'k', linestyle = '-', linewidth = 0.01)
+    axarr[i].xaxis.set_major_locator(MultipleLocator(xscale * 60 * 60))
+    axarr[i].xaxis.set_major_formatter(FuncFormatter(lambda x, pos: seconds2timestamp(int(x/xscale))))
+    axarr[i].xaxis.set_minor_locator(MultipleLocator(xscale * 30 * 60))
+    axarr[i].set_xlim(0, xscale * max(times))
+
+# 1) # Jobs In Use
+
+plot_label = "In Use"
+plot_list = jobs
+color = 'black'
+
 ymax = int(max(jobs))
 yexp = len(str(ymax)) - 1
 if yexp != 0:
-    axarr[0].text(0, 1, u"\u00D7" + " 10^" + str(yexp), horizontalalignment='left', verticalalignment='bottom', transform=axarr[0].transAxes)
+    axarr[0].text(0, 1, u"\u00D7" + " 10^" + str(yexp), horizontalalignment = 'left', verticalalignment = 'bottom', transform = axarr[0].transAxes)
 axarr[0].yaxis.set_major_formatter(create_y_ticker(yscale, yexp))
-axarr[0].set_ylabel("# Jobs")
+axarr[0].set_ylabel("# Active Jobs")
 axarr[0].set_ylim(0, yscale * ymax)
-axarr[0].plot([xscale * x for x in times], [yscale * y for y in jobs], color = 'black', markersize = 1)
+axarr[0].plot([xscale * x for x in times], [yscale * y for y in plot_list], color = color, markersize = linewidth)
 
-axarr[1].grid(color = 'k', linestyle = '-', linewidth = 0.01)
-axarr[1].xaxis.set_major_locator(MultipleLocator(xscale * 60 * 60))
-axarr[1].xaxis.set_major_formatter(FuncFormatter(lambda x, pos: seconds2timestamp(int(x/xscale))))
-axarr[1].xaxis.set_minor_locator(MultipleLocator(xscale * 60))
-axarr[1].set_xlim(0, xscale * max(times))
+# 2) # Steps
+
+plot_labels = ["Processing", "Writing", "Active"]
+plot_lists = [proc, write, steps]
+colors = ['blue', 'red', 'purple']
+
 ymax = int(max(steps))
 yexp = len(str(ymax)) - 1
 if yexp != 0:
-    axarr[1].text(0, 1, u"\u00D7" + " 10^" + str(yexp), horizontalalignment='left', verticalalignment='bottom', transform=axarr[1].transAxes)
+    axarr[1].text(0, 1, u"\u00D7" + " 10^" + str(yexp), horizontalalignment = 'left', verticalalignment = 'bottom', transform = axarr[1].transAxes)
 axarr[1].yaxis.set_major_formatter(create_y_ticker(yscale, yexp))
-axarr[1].set_ylabel("# Steps")
+axarr[1].set_ylabel("# Docs")
 axarr[1].set_ylim(0, yscale * ymax)
 for i in range(len(plot_lists)):
-    #axarr[i].grid(color = 'k', linestyle = '-', linewidth = 0.01)
-    #if i == len(axarr) - 1:
-    #    axarr[i].xaxis.set_major_locator(MultipleLocator(xscale * 60 * 60))
-    #    axarr[i].xaxis.set_major_formatter(FuncFormatter(lambda x, pos: seconds2timestamp(int(x/xscale))))
-    #    axarr[i].xaxis.set_minor_locator(MultipleLocator(xscale * 60))
-    #    axarr[i].set_xlim(0, xscale * max(times))
-        #axarr[i].xaxis.set_minor_formatter(FuncFormatter(lambda x, pos: seconds2timestamp(int(x))))
-    #axarr[i].set_yticks(np.arange(0, ypixels[i], yscale))
-    #axarr[i].set_yticklabels(step_labels)
-    #axarr[i].yaxis.set_major_locator(LinearLocator(10))
-    #ymax = int(max(plot_lists[i]))
-    #yexp = len(str(ymax)) - 1
-    #if yexp != 0:
-    #    axarr[i].text(0, 1, u"\u00D7" + " 10^" + str(yexp), horizontalalignment='left', verticalalignment='bottom', transform=axarr[i].transAxes)
-    #axarr[i].yaxis.set_major_formatter(create_y_ticker(yscale, yexp))
-    #axarr[i].set_ylabel(plot_labels[i])
-    #axarr[i].set_ylim(0, yscale * max(plot_lists[i]))
-    #axarr[i].scatter([xscale * x for x in times], [yscale * y for y in plot_lists[i]], s = 1)
-    axarr[1].plot([xscale * x for x in times], [yscale * y for y in plot_lists[i]], color = colors[i], markersize = 1, label = plot_labels[i])
+    axarr[1].plot([xscale * x for x in times], [yscale * y for y in plot_lists[i]], color = colors[i], markersize = linewidth, label = plot_labels[i])
+
+axarr[1].legend(bbox_to_anchor = (0.1, 1.21, 0.9, .102), loc = 'upper left', ncol = 3, mode = "expand", borderaxespad = 0.)
+
+# 3) # Steps Processed/Written
+
+plot_labels = ["Processed", "Written"]
+plot_lists = [proc_done, write_done]
+colors = ['orange', 'green']
+
+for i in range(len(plot_lists)):
+    ymax = int(max(plot_lists[i]))
+    yexp = len(str(ymax)) - 1
+    if yexp != 0:
+        axarr[i + 2].text(0, 1, u"\u00D7" + " 10^" + str(yexp), horizontalalignment = 'left', verticalalignment = 'bottom', transform = axarr[i + 2].transAxes)
+    axarr[i + 2].yaxis.set_major_formatter(create_y_ticker(yscale, yexp))
+    axarr[i + 2].set_ylabel("# Docs " + plot_labels[i])
+    axarr[i + 2].set_ylim(0, yscale * ymax)
+    axarr[i + 2].fill_between([xscale * x for x in times], 0, [yscale * y for y in plot_lists[i]], color = colors[i], linewidth = linewidth)
+    #axarr[i + 2].fill_between([xscale * x for x in times], 0, [yscale * (ymax - y) if y < 5 else 0 for y in plot_lists[i]], color = colors[i], linewidth = linewidth)
 
 #plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
-plt.legend(bbox_to_anchor = (0., -0.4, 1., .102), loc = 'upper left', ncol = 3, mode = "expand", borderaxespad = 0.)
-plt.subplots_adjust(bottom = 0.2)
+#plt.legend(bbox_to_anchor = (0., -0.4, 1., .102), loc = 'upper left', ncol = 2, mode = "expand", borderaxespad = 0.)
+#plt.subplots_adjust(bottom = 0.2)
+
+plt.subplots_adjust(hspace = 0.4)
 
 with PdfPages(out_path + "/" + out_file_name) as pdf:
     pdf.savefig(fig)
